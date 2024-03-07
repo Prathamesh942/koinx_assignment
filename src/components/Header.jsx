d7d62817728ac42124a084881f6b94106a74cd67
prathamesh <prathameshsk94@gmail.com>
2024-03-07 11:21:37 +0530

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className=" fixed flex justify-between w-[100%] px-[3vw] items-center bg-white shadow-sm py-2">
+      <img className=" w-16" src="public\assets\images\logo.svg" alt="" />
+      <nav className=" flex gap-5 font-medium">
+        <button>Crypto Taxes</button>
+        <button>Free Tools</button>
+        <button>Resource Center</button>
+        <button className=" bg-[#266BEB] p-2 px-6 rounded-lg text-white">
+          Get started
+        </button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/Cryptocurrency.jsx
+++ src/pages/Cryptocurrency.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Chart from "../components/Chart";
 import Performance from "../components/Performance";
+import Header from "../components/Header";
 
 const Cryptocurrency = ({ coin }) => {
   return (
-    <div className=" w-screen h-screen bg-[#EFF2F5] px-[3vw]">
-      {/* <Chart coin={coin} /> */}
-      <Performance coin={coin} />
+    <div>
+      <Header />
+      <div className=" w-screen h-screen bg-[#EFF2F5] px-[3vw]">
+        <Chart coin={coin} />
+        <Performance coin={coin} />
+      </div>
     </div>
   );
 };
